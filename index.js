ba4784f8ec0b81e65782100d92b540f95222106b
Vincent Sanders <vincesanders@hotmail.com>
2019-11-12 12:51:02 -0600

    I completed the 21st, 22nd, 23rd & 24th challenge.

--- index.js
+++ index.js
@@ -286,8 +286,16 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max) {
+  let count = 0;
+  return function counter() {
+    if (count < max) {
+      return count++;
+    } else if (count === max) {
+      count = 0;
+      return max;
+    }
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
