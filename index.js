349e5e2e84ba73b10c042cd1c4c3323cf9161950
Vincent Sanders <vincesanders@hotmail.com>
2019-11-12 11:43:16 -0600

    I completed the 6th, 7th & 8th challenge.

--- index.js
+++ index.js
@@ -180,8 +180,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let runnersFullNames = [];
+  runners.forEach(function(item, index) {
+    runnersFullNames[index] = `${item.last_name}, ${item.first_name}`
+  });
+  return runnersFullNames;
 }
 
 /**
