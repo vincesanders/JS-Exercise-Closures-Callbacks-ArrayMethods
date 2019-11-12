802b6412c7b8d2b252e0fc701623c688c8f2d572
Vincent Sanders <vincesanders@hotmail.com>
2019-11-12 11:20:16 -0600

    I completed the 3rd challenge.

--- index.js
+++ index.js
@@ -87,8 +87,10 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  return callback(numberList.reduce(function(sum, item) {
+    return sum += item;
+  }, 0));
 }
 
 /**
