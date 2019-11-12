474c6787677fdc57a3ecf562890303fe238c2c29
Vincent Sanders <vincesanders@hotmail.com>
2019-11-12 13:16:30 -0600

    I completed all the challenges including the STRETCH challenge.

--- index.js
+++ index.js
@@ -158,8 +158,10 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(list.filter(function(item, index) {
+    return list.indexOf(item) === index;
+  }));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
