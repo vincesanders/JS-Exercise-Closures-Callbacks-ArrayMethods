111e62d1efce2c116e598bb459e0f5c8df632d7e
Vincent Sanders <vincesanders@hotmail.com>
2019-11-12 12:08:06 -0600

    I completed the 12th & 13th challenge.

--- index.js
+++ index.js
@@ -219,8 +219,10 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(function(item) {
+    return (item.shirt_size === tShirtSize);
+  });
 }
 
 /**
